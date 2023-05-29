d4877a2b0a9e7e9d2186b7fd28724f6fc812645c
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2023-05-29 19:52:13 -0300

    feat: create first UI to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,19 @@
 export function Footer() {
-  return <footer>Seloco pai logo um footer aqui</footer>;
+  return (
+    <footer
+      className='container px-12 py-5
+      bg-slate-200 dark:bg-gray-700
+      text-gray-700 dark:text-white'
+    >
+      <h2 className='font-bold text-3xl leading-snug text-center'>
+        Social Media
+      </h2>
+      <ul className='container m-4 flex justify-between items-center'>
+        <li>Email</li>
+        <li>Github</li>
+        <li>Instagram</li>
+        <li>Linkedin</li>
+      </ul>
+    </footer>
+  );
 }
